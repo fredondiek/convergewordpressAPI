37630bb566e4eacd7e702a1dffdeb4c66fdbdd3a
Fredrick Ondieki <fredndk@gmail.com>
2014-08-02 13:10:19 +0300

    cleaning

--- wordpressplugin/src/test/java/dk/i2m/converge/plugins/actions/wordpress/WordPresslServicesClientTest.java
+++ wordpressplugin/src/test/java/dk/i2m/converge/plugins/actions/wordpress/WordPresslServicesClientTest.java
@@ -89,26 +89,7 @@ public class WordPresslServicesClientTest extends TestCase {
         
         
         
-//         // Arrange
-//        String webAccesPath = "http://localhost:8282/photos";
-//        String path = "2014\\7\\3\\934151";
-//        Catalogue catalogue = new Catalogue();
-//        catalogue.setWebAccess(webAccesPath);
-//        MediaItem mediaItem = new MediaItem();
-//        mediaItem.setTitle("Test");
-//        mediaItem.setCatalogue(catalogue);
-//        MediaItemRendition rendition = new MediaItemRendition();
-//        rendition.setPath(path);
-//        rendition.setFilename("3.png");
-//        rendition.setMediaItem(mediaItem);
-//        mediaItem.getRenditions().add(rendition);
-//
-//        // Act
-//        String actual = rendition.getAbsoluteFilename();
-//
-//        // Assert
-//        String expected = "http://localhost:8282/photos/2014/7/3/934151/3.png";
-//        assertEquals(expected, actual);
+
     }
 
     /**
