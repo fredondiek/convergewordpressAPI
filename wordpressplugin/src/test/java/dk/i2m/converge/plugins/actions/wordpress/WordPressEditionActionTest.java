2ab58f8185d4e9915a442921f85f9ac319ccfaa8
Fredrick Ondieki <fredndk@gmail.com>
2014-08-09 22:04:44 +0300

    Wordpress Tests For file/Image upload to the Remote Wordpress

--- wordpressplugin/src/test/java/dk/i2m/converge/plugins/actions/wordpress/WordPressEditionActionTest.java
+++ wordpressplugin/src/test/java/dk/i2m/converge/plugins/actions/wordpress/WordPressEditionActionTest.java
@@ -1,181 +1,181 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package dk.i2m.converge.plugins.actions.wordpress;
-
-import dk.i2m.converge.core.content.NewsItemPlacement;
-import dk.i2m.converge.core.plugin.PluginContext;
-import dk.i2m.converge.core.workflow.Edition;
-import dk.i2m.converge.core.workflow.OutletEditionAction;
-import java.util.Date;
-import java.util.Map;
-import java.util.ResourceBundle;
-import junit.framework.TestCase;
-
-/**
- *
- * @author fred
- */
-public class WordPressEditionActionTest extends TestCase {
-    
-    public WordPressEditionActionTest(String testName) {
-        super(testName);
-    }
-    
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
-    
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
-
-    /**
-     * Test of execute method, of class WordPressEditionAction.
-     */
-    public void testExecute() {
-        System.out.println("execute");
-        PluginContext ctx = null;
-        Edition edition = null;
-        OutletEditionAction action = null;
-        WordPressEditionAction instance = new WordPressEditionAction();
-        instance.execute(ctx, edition, action);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of executePlacement method, of class WordPressEditionAction.
-     */
-    public void testExecutePlacement() {
-        System.out.println("executePlacement");
-        PluginContext ctx = null;
-        NewsItemPlacement placement = null;
-        Edition edition = null;
-        OutletEditionAction action = null;
-        WordPressEditionAction instance = new WordPressEditionAction();
-        instance.executePlacement(ctx, placement, edition, action);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of isSupportEditionExecute method, of class WordPressEditionAction.
-     */
-    public void testIsSupportEditionExecute() {
-        System.out.println("isSupportEditionExecute");
-        WordPressEditionAction instance = new WordPressEditionAction();
-        boolean expResult = false;
-        boolean result = instance.isSupportEditionExecute();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of isSupportPlacementExecute method, of class WordPressEditionAction.
-     */
-    public void testIsSupportPlacementExecute() {
-        System.out.println("isSupportPlacementExecute");
-        WordPressEditionAction instance = new WordPressEditionAction();
-        boolean expResult = false;
-        boolean result = instance.isSupportPlacementExecute();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getAvailableProperties method, of class WordPressEditionAction.
-     */
-    public void testGetAvailableProperties() {
-        System.out.println("getAvailableProperties");
-        WordPressEditionAction instance = new WordPressEditionAction();
-        Map expResult = null;
-        Map result = instance.getAvailableProperties();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getName method, of class WordPressEditionAction.
-     */
-    public void testGetName() {
-        System.out.println("getName");
-        WordPressEditionAction instance = new WordPressEditionAction();
-        String expResult = "";
-        String result = instance.getName();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getDescription method, of class WordPressEditionAction.
-     */
-    public void testGetDescription() {
-        System.out.println("getDescription");
-        WordPressEditionAction instance = new WordPressEditionAction();
-        String expResult = "";
-        String result = instance.getDescription();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getVendor method, of class WordPressEditionAction.
-     */
-    public void testGetVendor() {
-        System.out.println("getVendor");
-        WordPressEditionAction instance = new WordPressEditionAction();
-        String expResult = "";
-        String result = instance.getVendor();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getDate method, of class WordPressEditionAction.
-     */
-    public void testGetDate() {
-        System.out.println("getDate");
-        WordPressEditionAction instance = new WordPressEditionAction();
-        Date expResult = null;
-        Date result = instance.getDate();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getBundle method, of class WordPressEditionAction.
-     */
-    public void testGetBundle() {
-        System.out.println("getBundle");
-        WordPressEditionAction instance = new WordPressEditionAction();
-        ResourceBundle expResult = null;
-        ResourceBundle result = instance.getBundle();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getAbout method, of class WordPressEditionAction.
-     */
-    public void testGetAbout() {
-        System.out.println("getAbout");
-        WordPressEditionAction instance = new WordPressEditionAction();
-        String expResult = "";
-        String result = instance.getAbout();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-}
+///*
+// * To change this template, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package dk.i2m.converge.plugins.actions.wordpress;
+//
+//import dk.i2m.converge.core.content.NewsItemPlacement;
+//import dk.i2m.converge.core.plugin.PluginContext;
+//import dk.i2m.converge.core.workflow.Edition;
+//import dk.i2m.converge.core.workflow.OutletEditionAction;
+//import java.util.Date;
+//import java.util.Map;
+//import java.util.ResourceBundle;
+//import junit.framework.TestCase;
+//
+///**
+// *
+// * @author fred
+// */
+//public class WordPressEditionActionTest extends TestCase {
+//    
+//    public WordPressEditionActionTest(String testName) {
+//        super(testName);
+//    }
+//    
+//    @Override
+//    protected void setUp() throws Exception {
+//        super.setUp();
+//    }
+//    
+//    @Override
+//    protected void tearDown() throws Exception {
+//        super.tearDown();
+//    }
+//
+//    /**
+//     * Test of execute method, of class WordPressEditionAction.
+//     */
+//    public void testExecute() {
+//        System.out.println("execute");
+//        PluginContext ctx = null;
+//        Edition edition = null;
+//        OutletEditionAction action = null;
+//        WordPressEditionAction instance = new WordPressEditionAction();
+//        instance.execute(ctx, edition, action);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of executePlacement method, of class WordPressEditionAction.
+//     */
+//    public void testExecutePlacement() {
+//        System.out.println("executePlacement");
+//        PluginContext ctx = null;
+//        NewsItemPlacement placement = null;
+//        Edition edition = null;
+//        OutletEditionAction action = null;
+//        WordPressEditionAction instance = new WordPressEditionAction();
+//        instance.executePlacement(ctx, placement, edition, action);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of isSupportEditionExecute method, of class WordPressEditionAction.
+//     */
+//    public void testIsSupportEditionExecute() {
+//        System.out.println("isSupportEditionExecute");
+//        WordPressEditionAction instance = new WordPressEditionAction();
+//        boolean expResult = false;
+//        boolean result = instance.isSupportEditionExecute();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of isSupportPlacementExecute method, of class WordPressEditionAction.
+//     */
+//    public void testIsSupportPlacementExecute() {
+//        System.out.println("isSupportPlacementExecute");
+//        WordPressEditionAction instance = new WordPressEditionAction();
+//        boolean expResult = false;
+//        boolean result = instance.isSupportPlacementExecute();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getAvailableProperties method, of class WordPressEditionAction.
+//     */
+//    public void testGetAvailableProperties() {
+//        System.out.println("getAvailableProperties");
+//        WordPressEditionAction instance = new WordPressEditionAction();
+//        Map expResult = null;
+//        Map result = instance.getAvailableProperties();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getName method, of class WordPressEditionAction.
+//     */
+//    public void testGetName() {
+//        System.out.println("getName");
+//        WordPressEditionAction instance = new WordPressEditionAction();
+//        String expResult = "";
+//        String result = instance.getName();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getDescription method, of class WordPressEditionAction.
+//     */
+//    public void testGetDescription() {
+//        System.out.println("getDescription");
+//        WordPressEditionAction instance = new WordPressEditionAction();
+//        String expResult = "";
+//        String result = instance.getDescription();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getVendor method, of class WordPressEditionAction.
+//     */
+//    public void testGetVendor() {
+//        System.out.println("getVendor");
+//        WordPressEditionAction instance = new WordPressEditionAction();
+//        String expResult = "";
+//        String result = instance.getVendor();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getDate method, of class WordPressEditionAction.
+//     */
+//    public void testGetDate() {
+//        System.out.println("getDate");
+//        WordPressEditionAction instance = new WordPressEditionAction();
+//        Date expResult = null;
+//        Date result = instance.getDate();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getBundle method, of class WordPressEditionAction.
+//     */
+//    public void testGetBundle() {
+//        System.out.println("getBundle");
+//        WordPressEditionAction instance = new WordPressEditionAction();
+//        ResourceBundle expResult = null;
+//        ResourceBundle result = instance.getBundle();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getAbout method, of class WordPressEditionAction.
+//     */
+//    public void testGetAbout() {
+//        System.out.println("getAbout");
+//        WordPressEditionAction instance = new WordPressEditionAction();
+//        String expResult = "";
+//        String result = instance.getAbout();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//}
